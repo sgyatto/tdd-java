9171acf86ec345d79f4f891dad46bbfb13d53504
sgyatto <42696711+sgyatto@users.noreply.github.com>
2018-11-04 22:45:34 +0900

    通貨用のフィールドを定義

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -1,11 +1,13 @@
 package money;
 
 class Dollar extends Money {
+    private String currency;
     Dollar(int amount) {
         this.amount = amount;
+        currency = "USD";
     }
     String currency() {
-        return "USD";
+        return currency;
     }
     Money times(int multiplier) {
         return new Dollar(amount * multiplier);
--- src/main/java/money/Franc.java
+++ src/main/java/money/Franc.java
@@ -1,11 +1,13 @@
 package money;
 
 class Franc extends Money {
+    private String currency;
     Franc(int amount) {
         this.amount = amount;
+        currency = "CHF";
     }
     String currency() {
-        return "CHF";
+        return currency;
     }
     Money times(int multiplier) {
         return new Franc(amount * multiplier);
