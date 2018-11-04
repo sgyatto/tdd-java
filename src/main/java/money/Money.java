3f97414c89986308cb49f109e0e9b36a0a0d7446
sgyatto <42696711+sgyatto@users.noreply.github.com>
2018-11-05 00:58:37 +0900

    Moneyからabstractを除去（テスト失敗）

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -1,13 +1,15 @@
 package money;
 
-abstract class Money {
+class Money {
     protected int amount;
     protected String currency;
     Money(int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
     }
-    abstract Money times(int multiplier);
+    Money times(int multiplier) {
+        return null;
+    };
     String currency() {
         return currency;
     }
