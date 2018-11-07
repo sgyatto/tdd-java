25764b1278f1f90378334966d50ce39e4d97390a
sgyatto <42696711+sgyatto@users.noreply.github.com>
2018-11-08 02:19:30 +0900

    メソッドの引き上げ

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -13,6 +13,9 @@ class Money implements Expression {
     Expression plus(Money addend) {
         return new Sum(this, addend);
     }
+    public Money reduce(String to) {
+        return this;
+    }
     String currency() {
         return currency;
     }
