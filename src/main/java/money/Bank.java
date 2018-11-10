6859fcc161006ad8497780548276d84484d08903
sgyatto <42696711+sgyatto@users.noreply.github.com>
2018-11-11 01:56:21 +0900

    Bankにratesフィールドを定義
    addRateでratesに要素を追加するようにした

--- src/main/java/money/Bank.java
+++ src/main/java/money/Bank.java
@@ -1,11 +1,15 @@
 package money;
 
+import java.util.Map;
+import java.util.HashMap;
+
 class Bank {
+    private Map<Pair, Integer> rates = new HashMap<>();
     Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
-
     void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), rate);
     }
     int rate(String from, String to) {
         return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
