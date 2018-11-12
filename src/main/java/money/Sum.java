8534dab2f0d224adfa542621f0362599d243ac9b
sgyatto <42696711+sgyatto@users.noreply.github.com>
2018-11-12 23:57:25 +0900

    Sumのplusを定義（コンパイルを通すための仮実装）

--- src/main/java/money/Sum.java
+++ src/main/java/money/Sum.java
@@ -8,6 +8,9 @@ class Sum implements Expression {
         this.augend = augend;
         this.addend = addend;
     }
+    public Expression plus(Expression addend) {
+        return null;
+    }
     public Money reduce(Bank bank, String to) {
         int amount = augend.reduce(bank, to).amount
                 + addend.reduce(bank, to).amount;
